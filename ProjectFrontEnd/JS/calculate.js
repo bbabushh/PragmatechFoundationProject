f729b900fe87f8854881ad2e493198914b19fd7d
Zohrab Seyidli <seyidlizohrab@gmail.com>
2021-11-28 23:20:28 +0400

    GitHub organize

---
+++ ProjectFrontEnd/JS/calculate.js
@@ -0,0 +1,18 @@
+let task1=90
+let task1=70
+project=true
+
+
+let ortalama = (task1 + task2) / 2
+if(ortalama < 70){
+    alert("fail")
+}
+else{
+    if(ortalama>90 && project==true){
+        alert("siz qizil mezunsunuz tebrikler")
+    }
+    else{
+        alert("siz fail oldunuz")
+    }
+
+}
