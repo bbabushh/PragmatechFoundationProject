8f8b095318b42e0893c3c724df38e9187db263fa
Zohrab Seyidli <seyidlizohrab@gmail.com>
2021-12-03 22:57:23 +0400

    GitHub organize

--- ProjectFrontEnd/JS/js/app.js
+++ ProjectFrontEnd/JS/js/app.js
@@ -1,3 +1,3 @@
-//      source cod
-
- console.log("hello world");
+let ilkEded = prompt("ilk ededi daxil edin :")
+let ikinciEded = prompt("ikinci ededi daxil edin:")
+alert(Number(ilkEded)+Number(ikinciEded))
--- ProjectFrontEnd/JS/js/calculate.js
+++ ProjectFrontEnd/JS/js/calculate.js
@@ -1,4 +1,24 @@
 let ilkEded = prompt("ilk ededi daxil edin :")
 let ikinciEded = prompt("ikinci ededi daxil edin :")
 
-alert(Number(ilkEded)+Number(ikinciEded))
+if(ilkEded*1==ilkEded && ikinciEded*1==ikinciEded){
+    let emeliyyat = prompt("emeliyyat isaresini daxil edin")
+
+    
+    if(emeliyyat == "+"){
+        
+        alert(ilkEded*1+ikinciEded*1)
+    }
+    if(emeliyyat=="-"){
+        alert(ilkEded-ikinciEded)
+    }
+    if(emeliyyat=="*"){
+        alert(ilkEded*ikinciEded)
+    }
+    if(emeliyyat=="/"){
+        alert(ilkEded/ikinciEded)
+    }
+}
+else{
+    alert("eded daxil etmeyi oyren")
+}
