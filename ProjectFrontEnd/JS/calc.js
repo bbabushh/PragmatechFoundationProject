e53e2b0d0b7b2a27315fc46b958e180a84bb7f4e
Zohrab Seyidli <seyidlizohrab@gmail.com>
2021-12-04 23:46:23 +0400

    GitHub organize

---
+++ ProjectFrontEnd/JS/calc.js
@@ -0,0 +1,14 @@
+// let a=1
+// while(a<100){
+//     console.log(a);
+//     a++
+// }
+// for(let a = 1;a<1000;a=a+2){
+//     console.log(a);
+// }
+for(let a = 1;a<1000;a=a++){
+    if(a%2==0);
+    console.log(a);
+    }
+    
+}
