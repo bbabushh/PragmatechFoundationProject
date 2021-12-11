1ab09b8d9885adc016b4f8dce9307a29e7069b43
Zohrab Seyidli <seyidlizohrab@gmail.com>
2021-12-11 23:38:12 +0400

    calc.js  changes

--- ProjectFrontEnd/JS/calc.js
+++ ProjectFrontEnd/JS/calc.js
@@ -1,14 +1,8 @@
-// let a=1
-// while(a<100){
-//     console.log(a);
-//     a++
-// }
-// for(let a = 1;a<1000;a=a+2){
-//     console.log(a);
-// }
-for(let a = 1;a<1000;a=a++){
-    if(a%2==0);
+let a=1
+while(a<100){
     console.log(a);
-    }
-    
+    a++
+}
+for(let a = 1;a<1000;a=a+2){
+    console.log(a);
 }
