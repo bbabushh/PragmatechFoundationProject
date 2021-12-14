79f8d7a625c1d59fe6a7743101c4fede9cc05a0d
Zohrab Seyidli <seyidlizohrab@gmail.com>
2021-12-14 23:58:30 +0400

    object stynax example

---
+++ JS/app.js
@@ -0,0 +1,28 @@
+let z_ad= "Zohrab"
+let z_soyad="Seyidli"
+let z_email="seyidlizohrab@gmail.com"
+let z_ish=function(){
+    return "babush  hergun kod yazir"
+}
+
+let z_yemek_yeme=function(){
+   return "babush arada yemeye fasile ver" 
+
+}
+
+
+let z={
+    ad="Zohrab",
+    soyad="Seyidli",
+    email="seyidlizohrab@gmail.com",
+    z_ish=function(){
+        return "babush hergun kod yazir"
+    },
+    z_yemek_yeme=function(){
+        return "babush arada yemeye fasile ver"
+    }
+    }
+
+
+}
+console.log(z.soyad);
